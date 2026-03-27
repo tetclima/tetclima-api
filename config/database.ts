12ade1bd6f018dc4f199d6e936cd8053f751f80d
Martin Nikov <149877218+mnikov00@users.noreply.github.com>
2026-03-27 13:00:11 +0200

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -45,7 +45,7 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
     },
     sqlite: {
       connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', 'database/data.db')),
       },
       useNullAsDefault: true,
     },
