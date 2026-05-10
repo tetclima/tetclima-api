38cef5881416eecc04dd235cdc33c346321a0b11
Martin Nikov <149877218+mnikov00@users.noreply.github.com>
2026-05-10 23:32:58 +0300

    140 products added

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -445,7 +445,6 @@ export interface ApiGlobalGlobal extends Struct.SingleTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    defaultSeo: Schema.Attribute.Component<'shared.seo', false>;
     favicon: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
